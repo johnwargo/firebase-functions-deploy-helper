03a05040867fd8daa1f89a85ba9a2e10ad6a67ee
John M. Wargo <johnwargo@users.noreply.github.com>
2021-03-04 07:25:20 -0500

    working on startup process

--- ffdh.js
+++ ffdh.js
@@ -4,14 +4,15 @@ var boxen = require('boxen');
 var chalk = require('chalk');
 var fs = require('fs');
 var logger = require('cli-logger');
-var packageDotJSON = require('./package.json');
 var path = require('path');
 var program = require('commander');
 var shell = require('shelljs');
-var APP_NAME = 'Firebase Functions Deployment Helper';
+var packageDotJSON = require('./package.json');
+var APP_NAME = 'Firebase Functions Deployment Helper (ffdh)';
 var APP_AUTHOR = 'by John M. Wargo (https://johnwargo.com)';
 var CURRENT_PATH = process.cwd();
 var EXIT_HEADING = chalk.red('Exiting:');
+var firebaseConfigFile = 'firebase.json';
 var log = logger();
 function checkFile(filePath) {
     log.debug("checkFile(" + filePath + ")");
@@ -46,7 +47,7 @@ function checkDirectory(filePath) {
 }
 function isValidConfig() {
     log.info(chalk.yellow('\nValidating Firebase project'));
-    var filePath = path.join(CURRENT_PATH, '.firebaserc');
+    var filePath = path.join(CURRENT_PATH, firebaseConfigFile);
     if (!checkFile(filePath)) {
         log.info(EXIT_HEADING + (" Unable to locate the " + filePath + " file\n"));
         return false;
@@ -54,21 +55,22 @@ function isValidConfig() {
     else {
         log.info("Located " + filePath);
     }
-    filePath = path.join(CURRENT_PATH, 'firebase.json');
-    if (!checkFile(filePath)) {
-        log.info(EXIT_HEADING + (" Unable to locate the " + filePath + " file\n"));
-        return false;
+    var firebaseConfig = require("./" + firebaseConfigFile);
+    var functionsPath = firebaseConfig.functions.source;
+    if (functionsPath) {
+        filePath = path.join(CURRENT_PATH, functionsPath);
+        console.log("Determined Firebase functions folder: " + filePath);
+        if (!checkDirectory(filePath)) {
+            log.info(EXIT_HEADING + (" Unable to locate the " + filePath + " folder\n"));
+            return false;
+        }
+        else {
+            log.info("Located " + filePath);
+        }
     }
     else {
-        log.info("Located " + filePath);
-    }
-    filePath = path.join(CURRENT_PATH, 'functions');
-    if (!checkDirectory(filePath)) {
         log.info(EXIT_HEADING + (" Unable to locate the " + filePath + " folder\n"));
         return false;
-    }
-    else {
-        log.info("Located " + filePath);
     }
     var res = shell.which('firebase');
     if (res) {
@@ -88,17 +90,27 @@ function isValidConfig() {
         return false;
     }
 }
-console.log(boxen(APP_NAME, { padding: 1 }));
+console.log(APP_NAME);
 console.log(APP_AUTHOR);
 console.log("Version: " + packageDotJSON.version);
 program.version(packageDotJSON.version);
+program.option('-e,', 'Search the end of function names');
+program.option('-i,', '');
+program.option('-s,', '');
+program.option('-%', '');
 program.option('-d, --debug', 'Output extra information during operation');
-program.option('-u, --update', 'Update the Assets definition in the pubspec.yaml file');
+program.parse(process.argv);
+var options = program.opts();
+if (options.debug) {
+    console.log('Enabling debug mode');
+    log.level(log.DEBUG);
+}
+else {
+    log.level(log.INFO);
+}
+log.debug(program.opts());
 if (isValidConfig()) {
-    program.parse(process.argv);
-    var conf = program.debug ? log.DEBUG : log.INFO;
-    log.level(conf);
-    log.debug(program.opts());
+    console.log('woohoo!');
 }
 else {
     console.log(chalk.red('Exiting'));
--- src/ffdh.ts
+++ src/ffdh.ts
@@ -14,119 +14,127 @@ const boxen = require('boxen');
 const chalk = require('chalk');
 const fs = require('fs');
 const logger = require('cli-logger');
-// https://stackoverflow.com/questions/9153571/is-there-a-way-to-get-version-from-package-json-in-nodejs-code
-const packageDotJSON = require('./package.json');
 const path = require('path');
 // https://www.npmjs.com/package/commander
 const program = require('commander');
 const shell = require('shelljs');
 
+// https://stackoverflow.com/questions/9153571/is-there-a-way-to-get-version-from-package-json-in-nodejs-code
+const packageDotJSON = require('./package.json');
+
 // constants
-const APP_NAME = 'Firebase Functions Deployment Helper';
+const APP_NAME = 'Firebase Functions Deployment Helper (ffdh)';
 const APP_AUTHOR = 'by John M. Wargo (https://johnwargo.com)';
 const CURRENT_PATH = process.cwd();
 const EXIT_HEADING = chalk.red('Exiting:');
+const firebaseConfigFile = 'firebase.json';
 
 var log = logger();
 
 function checkFile(filePath: string): boolean {
-    log.debug(`checkFile(${filePath})`);
-    try {
-        return fs.existsSync(filePath);
-    } catch (err) {
-        log.error(`checkFile error: ${err}`);
-        return false;
-    }
+  log.debug(`checkFile(${filePath})`);
+  try {
+    return fs.existsSync(filePath);
+  } catch (err) {
+    log.error(`checkFile error: ${err}`);
+    return false;
+  }
 }
 
 function checkDirectory(filePath: string): boolean {
-    log.debug(`checkDirectory(${filePath})`);
-    // does the folder exist?
-    if (fs.existsSync(filePath)) {
-        // Check to see if it's a folder
-        try {
-            let stats = fs.statSync(filePath);
-            if (stats) {
-                return stats.isDirectory;
-            } else {
-                return false;
-            }
-        } catch (err) {
-            log.error(`checkDirectory error: ${err}`);
-            return false;
-        }
-    } else {
+  log.debug(`checkDirectory(${filePath})`);
+  // does the folder exist?
+  if (fs.existsSync(filePath)) {
+    // Check to see if it's a folder
+    try {
+      let stats = fs.statSync(filePath);
+      if (stats) {
+        return stats.isDirectory;
+      } else {
         return false;
+      }
+    } catch (err) {
+      log.error(`checkDirectory error: ${err}`);
+      return false;
     }
+  } else {
+    return false;
+  }
 }
 
 function isValidConfig(): Boolean {
-    // Make sure this is a Flutter project
-    log.info(chalk.yellow('\nValidating Firebase project'));
+  // Make sure this is a Flutter project
+  log.info(chalk.yellow('\nValidating Firebase project'));
 
-    let filePath = path.join(CURRENT_PATH, '.firebaserc');
-    if (!checkFile(filePath)) {
-      log.info(EXIT_HEADING + ` Unable to locate the ${filePath} file\n`);
-      return false;
-    } else {
-      log.info(`Located ${filePath}`);
-    }
+  // Does the config file exist?
+  let filePath = path.join(CURRENT_PATH, firebaseConfigFile);
+  if (!checkFile(filePath)) {
+    log.info(EXIT_HEADING + ` Unable to locate the ${filePath} file\n`);
+    return false;
+  } else {
+    log.info(`Located ${filePath}`);
+  }
 
-    filePath = path.join(CURRENT_PATH, 'firebase.json');
-    if (!checkFile(filePath)) {
-      log.info(EXIT_HEADING + ` Unable to locate the ${filePath} file\n`);
-      return false;
-    } else {
-      log.info(`Located ${filePath}`);
-    }
-    
-    filePath = path.join(CURRENT_PATH, 'functions');
+  // load the Firebase config file
+  const firebaseConfig = require(`./${firebaseConfigFile}`);
+  // get the path for the functions folder
+  const functionsPath = firebaseConfig.functions.source;
+  if (functionsPath) {
+    // does the path exist?
+    filePath = path.join(CURRENT_PATH, functionsPath);
+    console.log(`Determined Firebase functions folder: ${filePath}`);
     if (!checkDirectory(filePath)) {
       log.info(EXIT_HEADING + ` Unable to locate the ${filePath} folder\n`);
       return false;
     } else {
       log.info(`Located ${filePath}`);
     }
-  
-    // is flutter installed?
-    let res = shell.which('firebase');
-    if (res) {
-      filePath = res.toString();
-      if (!filePath) {
-        log.info(EXIT_HEADING + ' Unable to locate the Firebase command\n');
-        return false;
-      } else {
-        log.info(`Firebase command found at ${path.dirname(filePath)}`);
-      }
-      log.info(chalk.green('We have a Firebase project'));
-      return true;
-    } else {
-      log.info(EXIT_HEADING + ' Unable to locate the Firebase command\n');
-      return false;
-    }
+  } else {
+    log.info(EXIT_HEADING + ` Unable to locate the ${filePath} folder\n`);
+    return false;
   }
 
-// Get started
-console.log(boxen(APP_NAME, { padding: 1 }));
+  // is the Firebase CLI installed?
+  let res = shell.which('firebase');
+  if (res) {
+    filePath = res.toString();
+    if (!filePath) {
+      log.info(EXIT_HEADING + ' Unable to locate the Firebase command\n');
+      return false;
+    } else {
+      log.info(`Firebase command found at ${path.dirname(filePath)}`);
+    }
+    log.info(chalk.green('We have a Firebase project'));
+    return true;
+  } else {
+    log.info(EXIT_HEADING + ' Unable to locate the Firebase command\n');
+    return false;
+  }
+}
+
+console.log(APP_NAME);
 console.log(APP_AUTHOR);
 console.log(`Version: ${packageDotJSON.version}`);
 
-// Get the version number from the package.json file
 program.version(packageDotJSON.version);
 program.option('-e,', 'Search the end of function names');
 program.option('-i,', '');
 program.option('-s,', '');
 program.option('-%', '');
-program.option('--debug', 'Output extra information during operation');
+program.option('-d, --debug', 'Output extra information during operation');
+program.parse(process.argv);
+const options = program.opts();
+if (options.debug) {
+  console.log('Enabling debug mode');
+  log.level(log.DEBUG);
+} else {
+  log.level(log.INFO);
+}
+// Write the command line options to the console
+log.debug(program.opts());
 
 if (isValidConfig()) {
-    // Process the command line arguments
-    program.parse(process.argv);
-    // Configure the logger
-    const conf = program.debug ? log.DEBUG : log.INFO;
-    log.level(conf);
-    log.debug(program.opts());
-
+  console.log('woohoo!');
 } else {
-    console.log(chalk.red('Exiting'));
+  console.log(chalk.red('Exiting'));
 }
